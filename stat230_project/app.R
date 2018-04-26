3c6708c0acc6f220162f374f6ab3c64a4e3e4b21
Andrea Boskovic 21 <aboskovic21@rstudio.amherst.edu>
2018-04-25 20:14:03 -0400

    andrea's changes

---
+++ stat230_project/app.R
@@ -0,0 +1,33 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(plotly)
+
+server <- function(input, output, session) {
+  
+  dat <- reactive(RodentsP[1:input$myslider,])
+  
+  output$myplot <- renderPlotly({
+    p <- ggplot(dat(), aes(MaxLongevitym, AdultBodyMassg)) + geom_point(color="purple")
+    p <- ggplotly(p)
+    p
+  })
+}
+
+ui <- basicPage(
+  
+  h3("Interactive Scatter Plot"),
+  sliderInput("myslider", "A slider:", min=100, max=700, value=10),
+  plotlyOutput("myplot")
+  
+)
+
+shinyApp(ui = ui, server = server)
+
